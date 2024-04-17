17904eb22b63abbb7a90f6d4169d62d84fa7dfce
EUIYOUNG HWANG <84139720+euiyounghwang@users.noreply.github.com>
2024-04-16 21:31:22 -0500

    Create service-start.sh

---
+++ service-start.sh
@@ -0,0 +1,20 @@
+
+
+#!/bin/bash
+set -e
+
+# Activate virtualenv && run serivce
+
+SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTDIR
+
+VENV=".venv"
+
+# Python 3.11.7 with Window
+if [ -d "$VENV/bin" ]; then
+    source $VENV/bin/activate
+else
+    source $VENV/Scripts/activate
+fi
+
+python -m uvicorn main:app --reload --host=0.0.0.0 --port=8001 --workers 4
