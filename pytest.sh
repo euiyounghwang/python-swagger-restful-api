1ccb514847285039fa5c516af28fd2b9fb3a9ab8
EUIYOUNG HWANG <84139720+euiyounghwang@users.noreply.github.com>
2024-04-16 21:32:51 -0500

    Create pytest.sh

---
+++ pytest.sh
@@ -0,0 +1,24 @@
+
+
+#!/bin/bash
+set -e
+
+# Activate virtualenv && run serivce
+
+SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd $SCRIPTDIR
+
+VENV=".venv"
+
+# Python 3.11.7 with Window
+if [ -d "$VENV/bin" ]; then
+    source $VENV/bin/activate
+else
+    source $VENV/Scripts/activate
+fi
+
+
+# py.test -v tests
+# poetry run py.test -v --junitxml=test-reports/junit/pytest.xml --cov-report html --cov tests/
+python -m  pytest -v ./tests
