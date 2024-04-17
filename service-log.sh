8d7aef422adc32728dd1798a78c333084991e31e
EUIYOUNG HWANG <84139720+euiyounghwang@users.noreply.github.com>
2024-04-16 21:31:45 -0500

    Create service-log.sh

---
+++ service-log.sh
@@ -0,0 +1,21 @@
+
+
+#!/bin/bash
+set -e
+
+# Activate virtualenv && run serivce
+
+SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd $SCRIPTDIR
+
+VENV=".venv"
+
+# Python 3.11.7 with Window
+if [ -d "$VENV/bin" ]; then
+    source $VENV/bin/activate
+else
+    source $VENV/Scripts/activate
+fi
+
+sudo journalctl -u swagger_es_api.service -f
