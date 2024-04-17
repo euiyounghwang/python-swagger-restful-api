18ac4fcc956d7bbb3a47ba88a0396bacc22de770
EUIYOUNG HWANG <84139720+euiyounghwang@users.noreply.github.com>
2024-04-16 21:36:31 -0500

    Create test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,31 @@
+
+import pytest
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_skip():
+    assert 1 != 1
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_api(mock_client):
+    response = mock_client.get("/cluster/health?es_url=http://localhost:9200")
+    assert response is not None
+    assert response.status_code == 200
+    assert 'cluster_name' in response.json()
+
+
+@pytest.mark.skip(reason="no way of currently testing this")    
+def test_index_mapping_compare_api(mock_client):
+    sample_payload = {
+        "source_mapping" : {"test_mapping" : "test"},
+        "target_mapping" : {"test_mapping" : "test"}
+    }
+     
+    response = mock_client.post("/index/mapping_compare_test", json=sample_payload)
+    assert response.status_code == 200
+
+    # It will be added diff login into service model on FastAPI F/W
+    assert response.json() == {
+        "response" : "ok"
+    }
